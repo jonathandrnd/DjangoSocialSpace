cd48eceef70f4114b70933e9bef6d7b0d7ac0cd3
jonathandrnd <jonathan.drnd@gmail.com>
2019-06-16 00:37:37 -0500

    first commit

---
+++ groups/tests.py
@@ -0,0 +1,24 @@
+"""
+from django.test import TestCase
+# Create your tests here.
+import pytest
+
+class TestProductModel(TestCase):
+
+    def test_save():
+        assert 500 == 500
+"""
+import unittest
+from django.test import Client
+
+class SimpleTest(unittest.TestCase):
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+
+    def test_details(self):
+        # Issue a GET request.
+        response = self.client.get('/groups/')
+
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code,200)
---
+++ spaceapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SpaceappConfig(AppConfig):
+    name = 'spaceapp'
